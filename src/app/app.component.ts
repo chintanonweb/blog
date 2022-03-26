9f105a4aec97ec6c02046868cf5ea16e115b2fb2
chintanonweb <chintandhokai97@gmail.com>
2022-03-26 15:13:50 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog';
+  loginForm= new FormGroup(
+    {
+      email : new FormControl(''),
+      password: new FormControl('')
+    }
+  )
+  loginUser(){
+    console.warn(this.loginForm.value)
+  }
 }
